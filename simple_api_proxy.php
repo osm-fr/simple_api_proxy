0feb60e531b4b9c00a56bd043aff5c8fd9540218
sletuffe <git@letuffe.org>
2013-01-19 17:40:54 +0100

    make it more generic by moving config to specific files for ease of update

--- simple_api_proxy.php
+++ simple_api_proxy.php
@@ -26,27 +26,7 @@ version 1.1 : add the <bounds> tag for map calls return
 version 1.0 : 1st release, it should work
 */
 
-// OverpassAPI server config (which URL to access)
-//$config['overpass_interpreter_url']="http://www.overpass-api.de/api/interpreter";
-$config['overpass_interpreter_url']="http://overpass.osm.rambler.ru/cgi/interpreter";
-//$config['overpass_interpreter_url']="http://api.openstreetmap.fr/oapi/interpreter";
-$config['overpass_interpreter_var_name']="data";
-
-// Set it to true for a temporary switch to a redirect/proxy 
-// You'd better switch to an other overpass api up there before doing that.
-// And only if no suitable overpassAPI exists, then swith this to true
-$config['force_proxy_mode']=false;
-
-// The base URL of the 0.6 API server
-// DO THINK TWICE AND MORE THAN DOUBLE CHECK BEFORE UNCOMMENTING THIS :
-//$config['live_osm_url']="http://api06.dev.openstreetmap.org/api";
-$config['live_osm_url']="http://api.openstreetmap.org/api";
-
-// Some funky text for the /capabilities call, this is only indicative for the client, the real limit is a timeout in case the request last too long
-// I believe even JOSM don't care about those values, so they are only here for API compliance
-$config['server_string']="osm-fr test proxy to the overpass api";
-$config['maximum_served_area']="2";
-$config['maximum_objects']=10000;
+require_once("config.php");
 
 // this is the real proxy part, should only be used for write access (but could do for READ access as well but is less usefull and risks of a blacklist)
 function forward_to_live_api($uri_without_api_in_case_of_forward)
