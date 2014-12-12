ef2fffef0ad2d87b2e39a7970bd52fb8c9a8dbbc
sletuffe <git@letuffe.org>
2014-12-12 16:19:08 +0100

    sample config with automatic http/https choosing

--- config-sample.php
+++ config-sample.php
@@ -12,9 +12,17 @@ $config['force_proxy_mode']=false;
 
 // The base URL of the 0.6 API server
 // DO THINK TWICE AND MORE THAN DOUBLE CHECK BEFORE UNCOMMENTING THIS :
-$config['live_osm_url']="http://api06.dev.openstreetmap.org/api";
-//$config['live_osm_url']="http://api.openstreetmap.org/api";
+//$live_osm_url_without_protocol="api.openstreetmap.org/api" 
+$live_osm_url_without_protocol="http://api06.dev.openstreetmap.org/api";
 
+// Dynamically choose https if we were called with https
+if (empty($_SERVER['HTTPS']))
+  $config['live_osm_url']="http://".$live_osm_url_without_protocol;
+else
+  $config['live_osm_url']="https://".$live_osm_url_without_protocol;
+    
+    
+    
 // Some funky text for the /capabilities call, this is only indicative for the client, the real limit is a timeout in case the request last too long
 // I believe even JOSM don't care about those values, so they are only here for API compliance
 $config['server_string']="--give me a name-- api 0.6 proxy tool to the overpass api";
